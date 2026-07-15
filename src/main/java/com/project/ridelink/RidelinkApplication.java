ef8608daa419ba9e3c22f716524f87c16d1645fe
Rudra Italiya <italiyas10@gmail.com>
2026-07-15 20:19:19 +0530

    Initial commit

---
+++ src/main/java/com/project/ridelink/RidelinkApplication.java
@@ -0,0 +1,13 @@
+package com.project.ridelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidelinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ridelink/RidelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ridelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
